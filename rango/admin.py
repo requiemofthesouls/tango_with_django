fe6cab0b1bb4f569754d13cb9abf181dc58fc9ba
Konstantin <32811996+requiemofthesouls@users.noreply.github.com>
2018-02-11 13:39:02 +0500

    end of the chapter 5. updated admin interface, models and have been created population script.

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from rango.models import Category, Page
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'views')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+
--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
+from rango.models import Category
+# from django.http import HttpResponse
 
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all of less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # Render the response and send it back!
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
